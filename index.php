cc658c3f1bbcb3a342b1d12012bdc5e58d504243
RealOne Digitals Hub <93271805+theridwanazeez@users.noreply.github.com>
2022-08-07 23:45:53 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,345 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    
+    <script src = "https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>  
+    <script src = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>  
+    <!--  Add bootstrap icon Library  -->  
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">  
+
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    
+    <title>RIDWAN AZEEZ (CV)</title>
+    
+    <style>
+	   .card-link p{padding: 5px; background-color: darkblue;}
+       .card-link a{color:white; text-decoration:none; }
+       .personal-span{padding-left: 1.5rem;}
+       .personal-span1{padding-left: 2.7rem;}
+       #personal-column{background-color: lightgray; border: none; border-radius: 0px;}
+       p{font-size:1rem; text-align: justify;}
+       .checked{color:yellow;}
+       #rateme1{margin-left:1rem} #rateme2{margin-left:3.8rem} #rateme3{margin-left:3.4rem}
+       #rateme4{margin-left:1.4rem} #rateme5{margin-left:3.1rem} #rateme6{margin-left:2.8rem}
+       h6{color: blue;}
+    .sm-icon{width: 2rem; height: 2rem;}  
+    .hobby-icon{width: 1.5rem; height: 1.5rem;}  
+
+
+/* Animations */
+       #full-name{
+           -webkit-animation-name: fullname;
+           -webkit-animation-duration: 3s;
+            animation-name: fullname;
+            animation-duration: 3s;
+       }
+       @-webkit-keyframes fullname{
+           0% {margin-left: 6rem; opacity:0.1;}
+           100% {margin-left: 0rem; opacity:1;}
+       }
+       @keyframes fullname{
+            0% {margin-left: 6rem; opacity:0.1;}
+           100% {margin-left: 0rem; opacity:1;}
+       }
+    
+        #skill-line{
+            -webkit-animation-name: skillline;
+           -webkit-animation-duration: 4s;
+            animation-name:skillline;
+            animation-duration: 4s;
+       }
+       @-webkit-keyframes skillline{
+           0% {margin-top: 4rem; opacity: 0.1;}
+           100% {margin-top: 0rem; opacity: 1;}
+       }
+       @keyframes skillline{
+            0% {margin-top: 4rem; opacity: 0.1;}
+           100% {margin-top: 0rem; opacity: 1;}
+       }
+
+       .img-fluid{
+        -webkit-animation-name: img;
+           -webkit-animation-duration: 4s;
+            animation-name:img;
+            animation-duration: 4s;
+       }
+       @-webkit-keyframes img{
+           0% {opacity: 0}
+           100% {opacity: 1}
+       }
+       @keyframes img{
+            0% {opacity: 0}
+           100% {opacity: 1}
+       }
+    </style>
+</head>
+<body>
+<header>
+
+    <div class="header" align="center" style="color:blue">
+        <P id="demo"></P><br>
+    </div>
+    
+</header>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5">
+                <div class="card bg-dark">
+                    <div align="center">
+                        <div class="card-image">
+                            <img class="img-fluid" src="image 1.jpg"/>
+                        </div>
+                        <div class="card-link">
+                            <a href="https://wa.me/2347045440979"><p style="text-align:center;"> <span>
+                            <img src="call icon.png" alt="icon" style="width: 30px; height: 30px;">
+                            </span>+234 704 544 0979</p></a>
+                            
+                            <a href="https://mail.google.com/mail/mu/mp/510/#co"><p style="text-align:center;"><span>
+                            <img src="email icon.png" alt="icon" style="width: 30px; height: 30px;">
+                            </span>theridwanazeez@gmail.com</p></a>
+
+                            <a href="https://linktr.ee/ridwanazeez"><p style="text-align:center;"><span>
+                            <img src="click icon.png" alt="icon" style="width: 30px; height: 30px;">
+                            </span>more contact options</p></a>
+                        </div>
+                    </div>
+                </div>
+            </div> <!-- col closed -->
+            
+            <div class="col-md-7">
+                <br>
+                <h2 style="color:blue;" id="full-name">AZEEZ RIDWAN AYOMO</h2>
+                <p id="skill-line"><b>WEB DESIGNER & DEVELOPER</b></p>
+                <p align="justify">My Name is Ridwanullah, at times called; RealOne. I am a Certified and Hardworking Digital Marketer who Specialized in Web Development, 
+                Branding & Designs and other related Digital Services. I have worked in various Technology-based companies and acquired lots of accolades
+                for being hardworking, smart and eagering to learn more on a daily basis. I always love getting busy with a computer system<i> I am having up to a 4years experience as a frontend Developer
+                and up to a year of experience developing backend. Backing it up is a 5years experience of being a Brand Identity creator (Creative Graphic Designer).</i>
+                I would love to work expertly and politely with you. <b>THANK YOU! for visiting my resume.</b></p>
+            <br>
+                <div class="container"> <!-- New container opened -->
+                    <div class="row" align="center">
+                        <div class="col-2">
+                            <a href="https://youtube.com/realonedigitals"><img class="img-fluid sm-icon" src="youtube icon1.png"></a>
+                        </div>
+                        <div class="col-2 offset-1">
+                            <a href="https://facebook.com/realonedigitals"><img class="img-fluid sm-icon" src="facebook icon.png"></a>
+                        </div>
+                        <div class="col-2 offset-1">
+                            <a href="https://instagram.com/realonedigitals"><img class="img-fluid sm-icon" src="instagram icon.png"></a>
+                        </div>
+                        <div class="col-2 offset-1">
+                            <a href="https://github.com/theridwanazeez"><img class="img-fluid sm-icon" src="github icon.png"></a>
+                        </div>
+                    </div><br>
+
+                    <div class="row" align="center">
+                        <div class="col-2">
+                            <a href="https://g.co/kgs/96AmRJ"><img class="img-fluid sm-icon" src="google icon.png"></a>
+                        </div>
+                        <div class="col-2 offset-1">
+                            <a href="https://selar.co/ridwanazeez"><img class="img-fluid sm-icon" src="course icon.png"></a>
+                        </div>
+                        <div class="col-2 offset-1">
+                            <a href="https://twitter.com/realonedigitals"><img class="img-fluid sm-icon" src="twitter icon.png"></a>
+                        </div>
+                        <div class="col-2 offset-1">
+                            <a href="https://wa.me/2347045440979"><img class="img-fluid sm-icon" src="whatsapp icon.png"></a>
+                        </div>
+                    </div>
+                </div> <!-- New container closed -->
+                <br>
+            </div>
+        </div> <!-- first row closed -->
+
+        
+
+        <div class="row">
+            <div class="col-md-5">
+                <div class="card" style="margin-top: 0rem;"> <!-- Personal card Open -->
+                    <div class="card-body" id="personal-column"> <!-- Card Body opened -->
+            <br>
+               <h4>PERSONAL</h4>
+               <p>Birthday <span class="personal-span1">: 15th Dec. 99</span><br>
+                Relatinship <span class="personal-span">: Single</span><br>
+                Nationality <span class="personal-span">: Nigerian</span><br>
+                Languages   <span class="personal-span">: English, Yoruba</span><br><br>
+            </p>
+            <h4>SKILLS</h4>
+               <p> <!-- Skill paragraph opened -->
+               Communication
+               <span id="rateme1" class="fa fa-star checked"></span>
+               <span id="rateme" class="fa fa-star checked"></span>
+               <span id="rateme" class="fa fa-star checked"></span>
+               <span id="rateme" class="fa fa-star checked"></span>
+               <span id="rateme" class="fa fa-star unchecked"></span>
+                </p>
+
+               <p>Creativity
+               <span id="rateme2" class="fa fa-star checked"></span>
+               <span id="rateme" class="fa fa-star checked"></span>
+               <span id="rateme" class="fa fa-star checked"></span>
+               <span id="rateme" class="fa fa-star checked"></span>
+               <span id="rateme" class="fa fa-star checked"></span>
+                </p>
+
+               <p>Teamwork
+               <span id="rateme3" class="fa fa-star checked"></span>
+               <span id="rateme" class="fa fa-star checked"></span>
+               <span id="rateme" class="fa fa-star checked"></span>
+               <span id="rateme" class="fa fa-star checked"></span>
+               <span id="rateme" class="fa fa-star checked"></span>
+                </p>
+
+                <p>Organizational
+               <span id="rateme4" class="fa fa-star checked"></span>
+               <span id="rateme" class="fa fa-star checked"></span>
+               <span id="rateme" class="fa fa-star checked"></span>
+               <span id="rateme" class="fa fa-star checked"></span>
+               <span id="rateme" class="fa fa-star unchecked"></span>
+               </p>
+
+                <p>Leadership
+               <span id="rateme5" class="fa fa-star checked"></span>
+               <span id="rateme" class="fa fa-star checked"></span>
+               <span id="rateme" class="fa fa-star checked"></span>
+               <span id="rateme" class="fa fa-star checked"></span>
+               <span id="rateme" class="fa fa-star checked"></span>
+               </p>
+
+                <p>Teamplayer
+               <span id="rateme6" class="fa fa-star checked"></span>
+               <span id="rateme" class="fa fa-star checked"></span>
+               <span id="rateme" class="fa fa-star checked"></span>
+               <span id="rateme" class="fa fa-star checked"></span>
+               <span id="rateme" class="fa fa-star checked"></span>
+               </p> <br>
+               </p> <!-- Skill paragraph closed -->
+            
+
+            <h4>REFERENCE</h4>
+                <h6>AZEEZ KAZEEM ADEMOLA</h6>
+               <p><b>Director, Demol Farms limited</b> <br>
+                <b>Phone:</b> +234 806 354 8499 <br>
+                <b>Email:</b> ademol@gmail.com<br><br>
+            </p>
+            
+                <h6>ILYAS SA'EED</h6>
+               <p><b>Director, Autlas Global Travels</b> <br>
+                <b>Phone:</b> +234 803 772 5411 <br>
+                <b>Email:</b> autlastravels@gmail.com <br><br>
+            </p>
+            </div> <!-- Card Body closed -->
+            </div> <!-- Personal card Closed -->
+            </div> <!-- col closed -->
+
+
+            
+            <div class="col-md-7">
+            <br>
+            <h5 style="background-color: lightgray; background-size:cover; text-align:center">EXPERIENCE</h5>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-4">
+                            <p><b>Lead Web Designer</b><br><span><p>2017 - 2019</p></span></p>
+                        </div>
+                        <div class="col-6 offset-2">
+                            <h6>VSL CONCEPTS</h6>
+                            <p>A creative website Designer at VSL Concepts Digital Marketing Agency.</p>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-4">
+                            <p><b>Senior Web Developer</b><br><span><p>2019 - 2021</p></span></p>
+                        </div>
+                        <div class="col-6 offset-2">
+                            <h6>WEBIIT</h6>
+                            <p>The seat of senior website Designer was gained at WEBIIT Digital Marketing Agency.</p>
+                        </div>
+                    </div> 
+
+                    <div class="row">
+                        <div class="col-4">
+                            <p><b> Ace Photoshop Designer</b><br><span><p>2015 - 2017</p></span></p>
+                        </div>
+                        <div class="col-6 offset-2">
+                            <h6>TATKO MULTI-LINKS</h6>
+                            <p>Ace(Master) Photoshop Designer was gained at TATKO Computer Technology ltd.</p>
+                        </div>
+                    </div>
+                </div> <!-- container closed -->
+
+                <!-- New Section -->
+            <div class="col-md-12">
+            <br>
+            <h5 style="background-color: lightgray; background-size:cover; text-align:center;">EDUCATION</h5>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-4">
+                            <p><b>Arulogun College</b><br><span><p>2010 - 2016</p></span></p>
+                        </div>
+                        <div class="col-6 offset-2">
+                            <h6>O'Level Certificate Program</h6>
+                            <p>Was achieved under proper mentorship of Arulogun College, Ovia North-East LG Edo State.</p>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-4">
+                            <p><b>Kwara State Polytechnic</b><br><span><p>2017 - 2019</p></span></p>
+                        </div>
+                        <div class="col-6 offset-2">
+                            <h6>OND in Computer Science</h6>
+                            <p>Studied in Kwara State Polytechnic along with lots of self-learning and practicals.</p>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-4">
+                            <p><b>Kwara State Polytechnic</b><br><span><p>2020 - 2022</p></span></p>
+                        </div>
+                        <div class="col-6 offset-2">
+                            <h6>HND in Computer Science</h6>
+                            <p>Was acquired under proper mentorship of Kwara State Polytechnic, Ilorin Kwara State.</p>
+                        </div>
+                    </div>
+                </div> <!-- container closed -->
+            </div> <!-- Section closed -->
+
+
+               <!-- New Section -->
+               <div class="col-md-12">
+                   <br>
+            <h5 style="background-color: lightgray; background-size:cover; text-align:center;">HOBBIES</h5>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-3">
+                            <img src="music icon.png" class="col-3 offset-1 hobby-icon"><br><p>musics</p>
+                        </div>
+                        <div class="col-3">
+                            <img src="coding icon.png" class="col-3 offset-1 hobby-icon"><br><p>coding</p>
+                        </div>
+                        <div class="col-3">
+                            <img src="gaming icon.png" class="col-3 offset-1 hobby-icon"><br><p>gaming</p>
+                        </div>
+                        <div class="col-3">
+                            <img src="dinning icon.png" class="col-3 offset-1 hobby-icon"><br><p>dinning</p>
+                        </div>
+                    </div>
+                </div> <!-- container closed -->
+            </div> <!-- Section closed -->
+        </div> <!-- second row closed -->
+
+    </div> <!-- container closed --> <br><br>
+
+    <div class="footer" align="center" style="color:blue">
+        <p>©️ Copyright: <span><a href="https://linktr.ee/ridwanazeez" style="font-size: 0.8rem; text-decoration:none;">Ridwan Azeez, 2022</a></span></p>
+    </div>
+</body>
